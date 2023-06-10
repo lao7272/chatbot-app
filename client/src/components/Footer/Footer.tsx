9a5e9e61601521507589f6c514958e69d6e6613a
Lao <estanislaolanus@gmail.com>
2023-06-10 11:24:10 -0300

    textarea responsive

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,9 +1,18 @@
-import React, {ChangeEvent, FormEvent, KeyboardEvent, useState} from 'react';
+import {ChangeEvent, FormEvent, KeyboardEvent, useState, useRef, useEffect, RefObject} from 'react';
 import "./Footer.css";
 import {FooterProps, Message} from "../../constants"
 
 export default function Footer({handleSetMessage, handleSetResponse, response}: FooterProps) {
     const [message, setMessage] = useState<string>('');
+    const textareaRef:RefObject<HTMLTextAreaElement> = useRef(null);
+    useEffect(() => {
+        const textarea = textareaRef.current;
+        if(!textarea) return;
+        textarea.style.height = "auto";
+        const height = textarea.scrollHeight;
+        textarea.style.height = `${height}px`;
+    }, [message])
+    
     function handleChange (e: ChangeEvent<HTMLTextAreaElement>) {
         const text = e.currentTarget.value;
         setMessage(text);
@@ -36,7 +45,7 @@ export default function Footer({handleSetMessage, handleSetResponse, response}: 
     return (
         <div className='chat-footer'>
             <form onSubmit={(e) => handleSubmit(e)}>
-                <textarea onChange={(e) => handleChange(e)} onKeyDown={e => preventEnter(e)} placeholder='Type a message'></textarea>
+                <textarea ref={textareaRef} onChange={(e) => handleChange(e)} onKeyDown={e => preventEnter(e)} placeholder='Type a message'></textarea>
                 {!response ? 
                 <button type='submit'><p>&#10148;</p></button> : 
                 <div className="chat-typing"></div>
